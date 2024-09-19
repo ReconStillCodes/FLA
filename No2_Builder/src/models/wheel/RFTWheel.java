ef711f89b51c0d7b887d37ff23b69888a99618a6
Matthew <m.matthew.muliawan@gmail.com>
2024-09-19 15:11:04 +0700

    Push Wheel

---
+++ No2_Builder/src/models/wheel/RFTWheel.java
@@ -0,0 +1,10 @@
+package models.wheel;
+
+public class RFTWheel extends Wheel {
+
+	public RFTWheel(String name, String color, int price) {
+		super(name, color, price);
+		// TODO Auto-generated constructor stub
+	}
+
+}
---
+++ No2_Builder/src/models/wheel/RadialWheel.java
@@ -0,0 +1,10 @@
+package models.wheel;
+
+public class RadialWheel extends Wheel {
+
+	public RadialWheel(String name, String color, int price) {
+		super(name, color, price);
+		// TODO Auto-generated constructor stub
+	}
+
+}
